14022268978dc86da7e31c24322da94d46ac5fc3
Yuji Maeda <maeda@cstyle.co.jp>
2018-07-26 18:49:08 +0900

    重複している駅を排除した

--- app/Models/StationMlitModel.php
+++ app/Models/StationMlitModel.php
@@ -53,7 +53,17 @@ class StationMlitModel extends ModelBase
             "FROM `mst_station_mlit` " .
                 "LEFT JOIN mst_city ON mst_station_mlit.city_id = mst_city.city_id " .
                 "LEFT JOIN tbl_trade_count ON mst_station_mlit.station_id = tbl_trade_count.area_id AND tbl_trade_count.station = 1 " .
-            "WHERE mst_city.city_id = ?";
+            "WHERE mst_city.city_id = ? " .
+            "GROUP BY " .
+                "mst_city.prefecture_id, " .
+                "mst_city.prefecture_name, " .
+                "mst_city.prefecture_alphabet, " .
+                "mst_city.city_id, " .
+                "mst_city.city_name, " .
+                "mst_city.city_alphabet, " .
+                "mst_station_mlit.station_id, " .
+                "mst_station_mlit.station_name, " .
+                "tbl_trade_count.trade_count ";
 
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$cityId]);
