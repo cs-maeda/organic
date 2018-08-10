b0ac50f8c5e831b81db4fa7dbfb5160b9d1d8436
Yuji Maeda <maeda@cstyle.co.jp>
2018-08-10 12:20:48 +0900

    不動産価格・不動産売買の相場、土地価格・土地売買の相場に路線価との相互リングを追加

--- resources/views/common/bodyFoot.blade.php
+++ resources/views/common/bodyFoot.blade.php
@@ -2,7 +2,7 @@
     <div class="inner">
         <ul class="footerMenu">
             @foreach($body['eachLink'] as $link)
-            <li><a href="{{$link['link']}}">{{$link['caption']}}</a></li>
+            <li><a href="{{$link['link']}}" target="_blank">{{$link['caption']}}</a></li>
             @endforeach
         </ul>
     </div>
