4e1856ee232a7de67a5ae8b9d4defc939b087124
Yuji Maeda <maeda@cstyle.co.jp>
2018-08-23 11:15:46 +0900

    グラフの高さを調整

--- resources/views/ginatonic/parts/common/bodyAverageGraph.blade.php
+++ resources/views/ginatonic/parts/common/bodyAverageGraph.blade.php
@@ -3,7 +3,7 @@
     <h2 class="titlePrg cal">@if ($body['where'] == 'index')日本全体の@else{{$body['areaCaptionOf']}}@endif地価公示価格平均推移</h2>
     @if ($body['where'] != 'index')<p>{{$body['areaCaption']}}全体の地価公示価格と日本全体の地価公示価格の平均値を比較したグラフです</p>@endif
     <div>
-        <canvas id="indexCharts" width=“300px” height=“300px”></canvas>
+        <canvas id="indexCharts" width=“300px” height=“200px”></canvas>
     </div>
     <p class="remark">※地価平均は全地点の公示価格平均値を算出、上昇率は新規測定地を除いた公示地点の平均上昇率</p>
 </section>
@@ -20,6 +20,7 @@
         });
 
         var ctx = document.getElementById("indexCharts").getContext('2d');
+        ctx.canvas.height = 100;
         var myChart = new Chart(ctx, {
             //グラフの種類
             type: 'line',
