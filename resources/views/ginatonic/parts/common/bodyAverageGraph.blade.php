d6aa5d47dffb9156b4a1bbb4757bf7463ab70ddc
kj_kaoru <kojima@csyule.co.jp>
2018-08-23 11:44:06 +0900

    トップページグラフ調整

--- resources/views/ginatonic/parts/common/bodyAverageGraph.blade.php
+++ resources/views/ginatonic/parts/common/bodyAverageGraph.blade.php
@@ -2,10 +2,10 @@
 <section class="prg averageGraph">
     <h2 class="titlePrg cal">@if ($body['where'] == 'index')日本全体の@else{{$body['areaCaptionOf']}}@endif地価公示価格平均推移</h2>
     @if ($body['where'] != 'index')<p>{{$body['areaCaption']}}全体の地価公示価格と日本全体の地価公示価格の平均値を比較したグラフです</p>@endif
-    <div>
+    <div class="graphArea">
         <canvas id="indexCharts" width=“300px” height=“200px”></canvas>
+        <p class="remark">※地価平均は全地点の公示価格平均値を算出、上昇率は新規測定地を除いた公示地点の平均上昇率</p>
     </div>
-    <p class="remark">※地価平均は全地点の公示価格平均値を算出、上昇率は新規測定地を除いた公示地点の平均上昇率</p>
 </section>
 <script>
 
@@ -38,15 +38,15 @@
                         //線のカーブ
                         lineTension: 0,
                         // //背景色
-                        // backgroundColor: "rgba(179,181,198,0.2)",
+                        backgroundColor: "rgba(255,255,255,1.0)",
                         //枠線の色
-                        borderColor: "rgba(179,181,198,1)",
+                        borderColor: "rgba(56,153,195,1.0)",
                         //結合点の枠線の色
-                        pointBorderColor: "rgba(179,181,198,1)",
+                        pointBorderColor: "rgba(56,153,195,1.0)",
                         // //結合点の背景色
-                        // pointBackgroundColor: "#fff",
+                        pointBackgroundColor: "#ffffff",
                         //結合点のサイズ
-                        pointRadius: 5,
+                        pointRadius: 4,
                         // //結合点のサイズ（ホバーしたとき）
                         // pointHoverRadius: 8,
                         // //結合点の背景色（ホバーしたとき）
@@ -56,12 +56,11 @@
                         // //結合点より外でマウスホバーを認識する範囲（ピクセル単位）
                         // pointHitRadius: 15,
                         //グラフのデータ
-                        data: data
+                        data: data,
                     }
                 ]
             },
             options: {
-                //細かなオプションを設定したい場合はここに記述
             }
         });
     }
