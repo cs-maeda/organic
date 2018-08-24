49523e51ee625de5c51d5eb3a15ff22032f22d04
kj_kaoru <kojima@csyule.co.jp>
2018-08-24 18:25:28 +0900

    テーブル表示調整

--- resources/views/ginatonic/parts/area/bodyTableLandPointList.blade.php
+++ resources/views/ginatonic/parts/area/bodyTableLandPointList.blade.php
@@ -1,18 +1,41 @@
-    <h2 class="titlePrg crown">{{$body['areaCaptionOf']}}地価公示価格一覧</h2>
-    <table class="tableRanking">
-        <colgroup span="1" class="col1">
-        <colgroup span="1" class="col2">
-        <colgroup span="1" class="col3">
+<script>
+    $().ready(function()
+    {
+        $('#point tr:nth-child(21)').attr('id', 'rowNumber').addClass('showLine');
+
+        $('#showPointData').on('click',
+        function(){
+            if($('#point').hasClass('show')){
+                $('#point').removeClass('show');
+                $(this).text('全てのデータを表示');
+            } else {
+                $('#point').addClass('show');
+                $(this).text('閉じる');
+            }
+        });
+        
+    });
+</script>
+
+
+<h2 id="pointTitle" class="titlePrg crown">{{$body['areaCaptionOf']}}地価公示価格一覧</h2>
+<table id="point" class="tableRanking">
+    <colgroup span="1" class="col1">
+    <colgroup span="1" class="col2">
+    <colgroup span="1" class="col3">
+    <tr>
+        <th>住所</th>
+        <th>価格（円/m<sup>2</sup>）</th>
+        <th>最寄り駅（バス停）</th>
+    </tr>
+    @foreach($body['pointList'] as $list)
         <tr>
-            <th>住所</th>
-            <th>価格（円/m<sup>2</sup>）</th>
-            <th>最寄り駅（バス停）</th>
+            <td>{{$list['address']}}</td>
+            <td class="value">{{$list['price']}}円/m<sup>2</sup></td>
+            <td>{{$list['station']}}</td>
         </tr>
-        @foreach($body['pointList'] as $list)
-            <tr>
-                <td>{{$list['address']}}</td>
-                <td class="value">{{$list['price']}}円/m<sup>2</sup></td>
-                <td>{{$list['station']}}</td>
-            </tr>
-        @endforeach
-    </table>
+    @endforeach
+</table>
+@if(count($body['pointList']) > 20)
+    <a href="#rowNumber" id="showPointData">全てのデータを表示</a>
+@endif
