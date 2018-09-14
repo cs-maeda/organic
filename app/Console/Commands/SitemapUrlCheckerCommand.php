e7d0bd222f0d559bdf53e43d708351bc66b20dab
tsukidate <tsukidate@lvn.co.jp>
2018-09-14 10:27:55 +0900

    URLチェッカーの実装

--- app/Console/Commands/SitemapUrlCheckerCommand.php
+++ app/Console/Commands/SitemapUrlCheckerCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Models\SitemapUrlModel;
 use Illuminate\Console\Command;
 
 class SitemapUrlCheckerCommand extends CommandBase
@@ -11,8 +12,9 @@ class SitemapUrlCheckerCommand extends CommandBase
      *
      * @var string
      */
-    protected $signature = 'check:sitemap_url';
+    protected $signature = 'check:sitemap_url {creatorId}';
 
+	protected $creatorId = null;
     /**
      * The console command description.
      *
@@ -37,10 +39,95 @@ class SitemapUrlCheckerCommand extends CommandBase
      */
     public function handle()
     {
-        //
+		//
+		$this->creatorId = $this->argument('creatorId');
+
+		$this->errorTrap();
+
+		try {
+			self::$errorInfo = 0;
+
+			$this->myEcho(' Start: Check sitemap_url.');
+
+			$this->urlCheck();
+
+			$this->send(self::MAIL_TO,
+				"[check:sitemap_url] {$this->creatorId}) successful",
+				'[check:sitemap_url] cron was executed successfully');
+
+			$this->myEcho(' End: Check sitemap_url.');
+
+			self::$errorInfo = 1;
+		}
+		catch (\Throwable $e)
+		{
+			$this->myEcho($e->getTraceAsString());
+
+			$this->sendErrorMessage($e->getTraceAsString());
+
+			self::$errorInfo = 1;
+
+			throw($e);
+		}
     }
 
-    protected function sendErrorMessage(string $message)
-    {
-    }
+	/**
+	 * @throws \Exception
+	 */
+	protected function urlCheck()
+	{
+		$ngItem = array();
+
+		// サイトマップURLを取得
+		foreach (SitemapUrlModel::where('creator_id', $this->creatorId)
+					 ->orderBy('url_id')
+					 ->cursor() as $result)
+		{
+			$urlList[$result['url_id']] = $result['url'];
+		}
+
+		// 404チェック
+		$context = stream_context_create(
+			array(
+				// HTTPステータス4xx, 5xxなどでWarnningエラーが出ないように設定
+				'http' => array('ignore_errors' => true),
+				// 無効な証明書でWarnningエラーが出ないよう設定
+				'ssl' => array(
+					'verify_peer' => false,
+					'verify_peer_name' => false,
+				),
+			)
+		);
+		foreach ($urlList as $urlId => $url) {
+			$res = @file_get_contents($url, false, $context);
+			// HTTPステータスが200以外の場合はNGフラグ
+			$pos = strpos($http_response_header[0], '200');
+			if ($pos === false) {
+				$ngItem[] = $urlId;
+			}
+		}
+
+		// SQL実行
+		if (count($ngItem) === 0) {
+			// creatorIdと一致するレコードのNGフラグを全て0に設定
+			SitemapUrlModel::where('creator_id', $this->creatorId)
+				->update(['ng_flag' => 0]);
+		} else {
+			// creatorIdと一致し、200以外が返ってきたレコードのNGフラグを全て1に設定
+			SitemapUrlModel::where('creator_id', $this->creatorId)
+				->whereIn('url_id', $ngItem)
+				->update(['ng_flag' => 1]);
+			// creatorIdと一致し、200が返ってきたレコードのNGフラグを全て0に設定
+			SitemapUrlModel::where('creator_id', $this->creatorId)
+				->whereNotIn('url_id', $ngItem)
+				->update(['ng_flag' => 0]);
+		}
+	}
+
+	protected function sendErrorMessage(string $message)
+	{
+		$this->send(self::MAIL_TO,
+			"[check:sitemap_url] {$this->creatorId} failed",
+			'[check:sitemap_url] cron ended in failure\n' . $message);
+	}
 }
